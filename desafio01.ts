fa7ce7620851eba518a3ffcc68c76b497b232081
gabrielrsh <gabriel.shiota@gmail.com>
2022-07-30 20:28:40 -0300

    Adiciona desafio 1

---
+++ desafio01.ts
@@ -0,0 +1,21 @@
+// Opcao 1
+let employee: {code: number, name: string} = {
+    code: 10,
+    name: "John"
+};
+// Opcao 2
+let employee2 = {
+    code: 10,
+    name: "John"
+}
+
+// Opcao 3
+interface Employee {
+    code: number;
+    name: string;
+}
+
+let employee3 : Employee = {
+    code: 10,
+    name: "John"
+}
