dc72008ab29d7e45c1b0256d1096b93f17bc5e4d
gabrielrsh <gabriel.shiota@gmail.com>
2022-07-31 15:50:38 -0300

    adiciona desafio 2 e desafio 3

---
+++ dist/desafios/desafio01.js
@@ -0,0 +1,15 @@
+"use strict";
+// Opcao 1
+let employee = {
+    code: 10,
+    name: "John"
+};
+// Opcao 2
+let employee2 = {
+    code: 10,
+    name: "John"
+};
+let employee3 = {
+    code: 10,
+    name: "John"
+};
---
+++ dist/desafios/desafio02.js
@@ -0,0 +1,26 @@
+"use strict";
+var Profissao;
+(function (Profissao) {
+    Profissao["Atriz"] = "Atriz";
+    Profissao["Padeiro"] = "Padeiro";
+})(Profissao || (Profissao = {}));
+let pessoa1 = {
+    nome: "Maria",
+    idade: 29,
+    profissao: Profissao.Atriz
+};
+let pessoa2 = {
+    nome: "Roberto",
+    idade: 19,
+    profissao: Profissao.Padeiro
+};
+let pessoa3 = {
+    nome: "Laura",
+    idade: 32,
+    profissao: Profissao.Atriz
+};
+let pessoa4 = {
+    nome: "Carlos",
+    idade: 19,
+    profissao: Profissao.Padeiro
+};
